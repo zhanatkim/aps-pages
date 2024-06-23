1d64aa928e914eaed2f3ec169bcb30c9175f67f2
Zhanat Kim <107250310+zhanatkim@users.noreply.github.com>
2024-06-23 22:40:47 +0500

    fix send.php and validate input

--- gulp/copyAssets.mjs
+++ gulp/copyAssets.mjs
@@ -6,7 +6,7 @@ const copyImages = () => gulp.src('source/img/**/*.{png,jpg,jpeg,webp,avif}', {b
 
 const copy = () =>
   gulp
-      .src(['source/**.html', 'source/fonts/**', 'source/img/**', 'source/favicon/**', 'source/data/**'], {
+      .src(['source/**.html', 'source/fonts/**', 'source/img/**', 'source/**.php', 'source/PHPMailer/**.php', 'source/favicon/**', 'source/data/**'], {
         base: 'source',
       })
       .pipe(gulp.dest('build'));
--- source/js/form/form-send-callbacks.js
+++ source/js/form/form-send-callbacks.js
@@ -1,36 +1,36 @@
-const success = {
-  'ru': 'Ваш запрос успешно отправлен.',
-  'en': 'Your request has been sent successfully.',
-  'kz': 'Сұрауыңыз сәтті жіберілді.',
-};
+// const success = {
+//   'ru': 'Ваш запрос успешно отправлен.',
+//   'en': 'Your request has been sent successfully.',
+//   'kz': 'Сұрауыңыз сәтті жіберілді.',
+// };
 
-const errors = {
-  'ru': 'Произошла ошибка отправки.',
-  'en': 'There was a sending error.',
-  'kz': 'Жіберу қатесі болды.',
-};
+// const errors = {
+//   'ru': 'Произошла ошибка отправки.',
+//   'en': 'There was a sending error.',
+//   'kz': 'Жіберу қатесі болды.',
+// };
 
 export const baseSendSuccess = (target) => {
   window.clearForm(target);
-  success.forEach((key, value) => {
-    if (document.querySelector('html').getAttribute('lang') === key) {
-      // eslint-disable-next-line no-console
-      console.log(value);
-    }
-  });
+  // success.forEach((key, value) => {
+  //   if (document.querySelector('html').getAttribute('lang') === key) {
+  //     // eslint-disable-next-line no-console
+  //     console.log(value);
+  //   }
+  // });
 
   // eslint-disable-next-line no-console
   console.log('Ваша форма успешно отправлена');
 };
 
 export const baseSendError = () => {
-  errors.forEach((key, value) => {
-    if (document.querySelector('html').getAttribute('lang') === key) {
-      console.log(document.querySelector('html').getAttribute('lang'));
-      // eslint-disable-next-line no-console
-      console.log(value);
-    }
-  });
+  // errors.forEach((key, value) => {
+  //   if (document.querySelector('html').getAttribute('lang') === key) {
+  //     console.log(document.querySelector('html').getAttribute('lang'));
+  //     // eslint-disable-next-line no-console
+  //     console.log(value);
+  //   }
+  // });
   // eslint-disable-next-line no-console
   console.error('Произошла ошибка отправки');
 };
